09353ec7cb8c77a7ade632d6ab493c0b59b71b89
rrgeorge21 <45045458+rrgeorge21@users.noreply.github.com>
2019-02-13 22:38:14 -0500

    Add files via upload

---
+++ Jedi Arkantos Rajax_files/css(2).php
@@ -0,0 +1,863 @@
+#header {
+border-top: 1px solid #555555;
+border-bottom: 1px solid #000;
+height: 121px;
+-moz-box-shadow:inset 0 0 10px #000000;
+-webkit-box-shadow:inset 0 0 10px #000000;
+box-shadow:inset 0 0 10px #000000;
+}
+
+.toplinks form input.loginbutton {
+background-color: #555555;
+-moz-border-radius: 4px;
+-webkit-border-radius: 4px;
+border-radius: 4px;
+padding: 1px 15px;
+}
+
+.toplinks form input.loginbutton:hover {
+border: 1px solid #555555;
+background-color: #202020;
+-moz-border-radius: 4px;
+-webkit-border-radius: 4px;
+border-radius: 4px;
+padding: 1px 15px;
+}
+
+.toplinks form input.textbox {
+border: 1px solid #555555;
+padding: 1px 4px;
+-moz-border-radius: 4px;
+-webkit-border-radius: 4px;
+border-radius: 4px;
+}
+
+.below_body {
+background: url(images/Seamus/gradients/docbot.png) bottom left repeat-x;
+}
+
+.notices li {
+border: 2px solid #202020;
+background: url(images/Seamus/gradients/back3.png) top left repeat-x;
+-moz-box-shadow: 0 0 2px #000;
+-webkit-box-shadow: 0 0 2px #000;
+box-shadow: 0 0 2px #000;
+-moz-border-radius: 10px;
+-webkit-border-radius: 10px;
+border-radius: 10px;
+padding: 4px 8px;
+margin: 4px;
+}
+
+.postbitlegacy .postbody, .eventbit .eventdetails .eventbody {
+border-bottom: 1px solid #000;
+}
+
+.thread_info h4  {
+border-top: 1px solid #555555;
+padding-top: 7px;
+padding-bottom: 12px;
+border-bottom: 1px solid #000;
+-moz-border-top-right-radius: 7px;
+-webkit-border-top-right-radius: 7px;
+border-top-right-radius: 7px;
+-moz-border-top-left-radius: 7px;
+-webkit-border-top-left-radius: 7px;
+border-top-left-radius: 7px;
+}
+
+.blockhead {
+padding-top: 7px;
+padding-bottom: 12px;
+border-top: 1px solid #555555;
+border-bottom: 1px solid #000;
+-moz-border-top-right-radius: 7px;
+-webkit-border-top-right-radius: 7px;
+border-top-right-radius: 7px;
+-moz-border-top-left-radius: 7px;
+-webkit-border-top-left-radius: 7px;
+border-top-left-radius: 7px;
+}
+
+.postbit .posthead, .postbitlegacy .posthead, .eventbit .eventhead {
+padding-top: 7px;
+padding-bottom: 12px;
+border-top: 1px solid #555555;
+border-bottom: 1px solid #000;
+-moz-border-top-right-radius: 7px;
+-webkit-border-top-right-radius: 7px;
+border-top-right-radius: 7px;
+-moz-border-top-left-radius: 7px;
+-webkit-border-top-left-radius: 7px;
+border-top-left-radius: 7px;
+}
+
+.threadlisthead  {
+border-top: 1px solid #555555;
+padding-top: 3px;
+padding-bottom: 8px;
+border-bottom: 1px solid #000;
+-moz-border-top-right-radius: 7px;
+-webkit-border-top-right-radius: 7px;
+border-top-right-radius: 7px;
+-moz-border-top-left-radius: 7px;
+-webkit-border-top-left-radius: 7px;
+border-top-left-radius: 7px;
+}
+
+#sidebar_container > ul {
+border-top: 1px solid #555555;
+}
+
+.cms_widget .block {
+border-top: 1px solid #555555;
+}
+
+.forum_info .blockbody {
+border-top: 1px solid #202020;
+background: #161616 url(images/Seamus/gradients/forumrow1.png) top left repeat-x;
+}
+
+.wgo_block .blockbody {
+border-top: 0;
+}
+
+.globalsearch .searchbutton {
+border: 1px solid #555555 !important;
+-moz-box-shadow: 0 0 2px #1a1a1a;
+-webkit-box-shadow: 0 0 2px #1a1a1a;
+box-shadow: 0 0 2px #1a1a1a;
+-moz-border-radius: 7px;
+-webkit-border-radius: 7px;
+border-radius: 7px;
+margin-left: -5px;
+margin-top: -2px;
+background: url(images/Seamus/gradients/back3.png) top left repeat-x;
+}
+
+.globalsearch { 
+width: 190px;
+}
+
+.globalsearch .buttoncontainer {
+float: left;
+}
+
+.wgo_block .blockhead, .forum_info .blockhead  {
+border-top: 1px solid #555555;
+padding-top: 7px;
+padding-bottom: 11px;
+border-bottom: 1px solid #000;
+-moz-border-top-right-radius: 7px;
+-webkit-border-top-right-radius: 7px;
+border-top-right-radius: 7px;
+-moz-border-top-left-radius: 7px;
+-webkit-border-top-left-radius: 7px;
+border-top-left-radius: 7px;
+}
+
+.forumhead {
+border-top: 1px solid #555555;
+padding-top: 5px;
+padding-bottom: 5px;
+border-bottom: 1px solid #000;
+-moz-border-top-right-radius: 7px;
+-webkit-border-top-right-radius: 7px;
+border-top-right-radius: 7px;
+-moz-border-top-left-radius: 7px;
+-webkit-border-top-left-radius: 7px;
+border-top-left-radius: 7px;
+}
+
+#crumbR  li {
+display: inline;
+list-style-type: none;
+padding: 0;
+margin: 0;
+}
+
+#crumbR ul {
+padding: 0;
+margin: 0;
+}
+
+#newcrumb, .footer {
+width: auto;
+border: 2px solid #202020;
+background: url(images/Seamus/gradients/back3.png) top left repeat-x;
+-moz-box-shadow: 0 0 2px #000;
+-webkit-box-shadow: 0 0 2px #000;
+box-shadow: 0 0 2px #000;
+-moz-border-radius: 10px;
+-webkit-border-radius: 10px;
+border-radius: 10px;
+padding: 4px 8px;
+margin: 4px;
+}
+
+#crumbL {
+float: left;
+}
+
+#crumbR {
+float: right;
+}
+
+#pagetitle {
+padding: 5px 10px 0px 10px;
+}
+
+.globalsearch input.textbox {
+margin-top: 3px;
+border: 1px solid #555555;
+padding: 3px;
+-moz-box-shadow: 0 0 2px #1a1a1a;
+-webkit-box-shadow: 0 0 2px #1a1a1a;
+box-shadow: 0 0 2px #1a1a1a;
+-moz-border-radius: 6px;
+-webkit-border-radius: 6px;
+border-radius: 6px;
+margin-right: -10px;
+}
+
+.forumhead + .childforum .L2:first-child .forumrow, .forumhead + .L2 .forumrow {
+border-top: 1px solid #000;
+}
+
+.forumbit_nopost .subforumdescription {
+border-top: 1px solid #000;
+border-bottom: 1px solid #000;
+}
+
+.navtabs li a.navtab {
+line-height: 34px;
+text-shadow: 1px 1px 1px #000;
+border: 0 important;
+border-right: 0 !important;
+}
+
+.navtabs li.selected a.navtab {
+margin-top: -2px;
+color: #fff;
+line-height: 38px;
+}
+
+.navtabs li a.navtab:hover {
+margin-top: -2px;
+background: url(images/Seamus/gradients/navselect.png) top center no-repeat;
+color: #fff;
+line-height: 38px;
+}
+
+.navtabs {
+border-top: 1px solid #555555;
+border-bottom: 1px solid #000;
+}
+
+.navtabs ul {
+border-top: 1px solid #262626;
+}
+
+#newnav {
+height: 30px;
+background: url(images/Seamus/gradients/header-back.png) bottom left repeat-x;
+padding-right:30px;
+border-bottom: 1px solid #000;
+}
+
+.toplinks {
+top: 4px;
+}
+
+#blueWrap {
+margin: 0;
+background-color: #1a1a1a;
+height:100%;
+}
+
+#blueRight {
+padding-right: 62px;
+background: url(images/Seamus/gradients/bordRight.png) top right repeat-y;
+}
+
+#blueLeft {
+padding-left: 62px;
+background: url(images/Seamus/gradients/bordLeft.png) top left repeat-y;
+}
+
+#bottomleft {
+background: url(images/Seamus/gradients/bottomleft.png) bottom left no-repeat;
+}
+
+#bottomright {
+background: url(images/Seamus/gradients/bottomright.png) bottom right no-repeat;
+}
+
+#topleft {
+background: url(images/Seamus/gradients/topleft.png) top left no-repeat;
+}
+
+#topright {
+background: url(images/Seamus/gradients/topright.png) top right no-repeat;
+}
+
+#bodytop {
+background: #131313 url(images/Seamus/gradients/bodytop.png) top left repeat-x;
+margin: 4px;
+}
+
+#bodybottom {
+background: url(images/Seamus/gradients/bodybottom.png) bottom left repeat-x;
+}
+
+#bodyleft {
+background: url(images/Seamus/gradients/bodyleft.png) left repeat-y;
+}
+
+#bodyright {
+background: url(images/Seamus/gradients/bodyright.png) right repeat-y;
+}
+
+.wysiwyg_block .blockrow:first-child {
+background-image: none;
+}
+
+#usercp_nav hr {
+background:none repeat scroll 0 0 #000;
+color:#000;
+height:1px;
+width:80%;
+}
+
+#forum_icon_legend {
+border: 1px solid #000;
+}
+
+.groupbit {
+border: 1px solid #000;
+}
+
+.wgo_block .section {
+border-top: 1px solid #202020;
+border-bottom: 1px solid #000;
+background: #161616 url(images/Seamus/gradients/forumrow1.png) top left repeat-x;
+}
+
+.thread_info .blockbody {
+background: #161616 url(images/Seamus/gradients/forumrow1.png) top left repeat-x;
+}
+
+.vb-tree-menu .ygtvitem {
+background-color:#1a1a1a;
+border: 0;
+}
+
+.alt1, .alt1Active, .alt2, .alt2Active {
+color: #ccc;
+}
+
+.groupbit:hover .postcontrols a {
+color: #fff;
+}
+
+.body_wrapper {
+margin: 10px 0 10px 0;
+background-color: transparent !important;
+}
+
+.toplinks ul.nouser li a {
+background: transparent;
+}
+
+.toplinks .logindetails {
+   background: transparent;
+}
+
+.toplinks ul.isuser li a:hover {
+background: transparent;
+}
+
+.toplinks ul.nouser li a:hover {
+   background: transparent;
+}
+
+.toplinks .notifications a.popupctrl:hover {
+   background: transparent;
+}
+
+.bppostbit {
+margin-top: 2px;
+padding: 3px;
+background: url(images/Seamus/gradients/back3.png) top left repeat-x;
+border: 1px solid #000;
+}
+
+#textbox {
+color #fff;
+}
+
+select {
+background-color: #000;
+color: #fff;
+border: 1px solid #202020;
+}
+
+div .forumrow:hover, .forumhead + .childforum .L2:first-child .forumrow:hover {
+background: #262626 url(images/Seamus/gradients/forumrow3.png) top left repeat-x;
+}
+
+.threadbit .nonsticky:hover, .threadbit .discussionrow:hover {
+background: #262626 url(images/Seamus/gradients/forumrow3.png) top left repeat-x;
+}
+
+.formcontrols .blockrow .editor_textbox textarea:focus {
+color: #fff;
+}
+
+.editor_textbox textarea,
+.editor_textbox iframe {
+color: #fff;
+}
+
+.editor_textbox.quickreply textarea, .editor_textbox.quickreply iframe, .editor_textbox iframe {
+color: #fff;
+background-color: #000;
+}
+
+.editor_controls .popupmenu .popupbody {
+background:#000 none repeat scroll 0 0;
+color: #fff;
+}
+
+.editor_controls .popupmenu .popupbody li > a {
+color:#fff;
+}
+
+.editor_controls .popupmenu .popupbody li > a:hover {
+color:#fff;
+background-color: #202020;
+}
+
+.editor_controls .popupmenu div.popupctrl {
+border:1px solid #202020;
+color:#fff;
+background-color:#000;
+}
+
+.editor_controls .popupmenu .popupctrl.active,
+.editor_controls .imagebutton_selected,
+.editor_controls .imagebutton_down,
+.editor_controls .imagebutton_hover {
+color: #fff;
+background-color: #000;
+}
+
+.editor_controls .popupmenu .popupctrl.active,
+.editor_controls .imagebutton_hover {
+background-color: #000000;
+color: #fff;
+}
+
+.editor_controls .imagebutton_selected {
+color: #fff;
+background-color: #000;
+}
+
+.editor_controls .imagebutton_down {
+background-color: #1a1a1a;
+}
+
+.editoroverlay input.textbox:focus,
+.editoroverlay textarea:focus {
+background-color: #000000;
+}
+
+#grouppagetitle {
+border: 1px solid #000;
+background: #1a1a1a;
+}
+
+#grouppagetitle h1 {
+color:#ccc;
+border-top: 1px solid #202020;
+}
+
+#grouppagetitle h1 a {
+color:#fff;
+}
+
+#uploaddisplay {
+border-bottom: 1px solid #000;
+}
+
+#uploaddisplay .blocksubhead {
+border: 1px solid #000;
+}
+
+#uploaddisplay ul {
+border: 1px solid #000;
+}
+
+#uploaddisplay .blockfoot.actionbuttons {
+border: 1px solid #000;
+}
+
+#content .blockrow.editpic .maineditor textarea {
+background: #1a1a1a;
+}
+
+#thumbnails li {
+background-color:#1a1a1a;
+}
+
+#menugroup h6 a {
+color:#ccc;
+}
+
+img.groupimage {
+background: #1a1a1a;
+}
+
+#pictures .blockbody .blockrow, #group_discussion_inlinemod_form .blockbody .blockrow {
+border-color:#000 #000 #000;
+border-style:solid;
+border-width:1px;
+}
+
+.textbox,
+textarea {
+background-color: #000;
+color: #fff;
+}
+
+.formcontrols .blockrow input.textbox:focus,
+.formcontrols .blockrow textarea:focus {
+color: #fff;
+}
+
+.bloglistinfo, .blogcatdisplayorder {
+background-color: #1a1a1a;
+}
+
+.blogcatdescription, .blogcatblogentries {
+background-color: #202020;
+}
+
+dl.tabslight dd a:hover {
+color: #000;
+}
+
+dl.tabslight dd.selected a:hover {
+color: #fff;
+}
+
+.bbcode_container div.bbcode_code, .bbcode_container pre.bbcode_code {
+border: 1px solid #000;
+color: #ccc;
+}
+
+blockquote.preview .bbcode_container div.bbcode_code, blockquote.preview .bbcode_container pre.bbcode_code {
+color: #ccc;
+}
+
+#searchtypeswitcher li a {
+background-color:#000;
+color:#FFF;
+
+}
+
+#searchtypeswitcher li.selected a {
+background-color:#1a1a1a;
+}
+
+#searchtypeswitcher li a:hover {
+background-color:#131313;
+}
+
+#searchtypeswitcher {
+border-bottom-color: #1a1a1a;
+}
+
+.editor_textbox textarea, .editor_textbox iframe {
+background-color: #000;
+color: #fff;
+}
+
+.wysiwyg_block iframe.textbox {
+background-color: #000;
+color: #fff;
+}
+
+.textbox, textarea, select {
+background-color: #000;
+color: #fff;
+}
+
+body.wysiwyg {
+background-color: #000;
+color: #fff;
+}
+
+.wysiwyg {
+background-color: #000;
+color: #fff;
+}
+
+legend {
+color: #fff;
+}
+
+input {
+background-color: #000;
+color: #fff;
+}
+
+li.sgicon .maincol, #sidebar li.sgicon .maincol {
+background-color: #1a1a1a;
+}
+
+.yui-skin-sam .yui-layout .yui-layout-unit div.yui-layout-bd {
+background-color: #1a1a1a;
+color: #fff;
+}
+
+.yui-skin-sam .yui-layout .yui-layout-unit div.yui-layout-hd h2, .yui-skin-sam .yui-layout .yui-layout-unit div.yui-layout-hd div {
+background-color: #202020;
+color: #fff;
+border: 1px solid #000;
+}
+
+#asset_attachment_list_footer, .yui-skin-sam .yui-layout .yui-layout-unit div.yui-layout-ft {
+background-color: #000;
+color: #fff;
+}
+
+.yui-skin-sam .yui-layout .yui-resize .yui-resize-handle-active, .yui-skin-sam .yui-layout-unit .yui-resize-handle, .yui-skin-sam .yui-layout-unit .yui-resize-handle-r {
+background-color: #000;
+}
+
+#upload_controls_container .upload_controls {
+background-color: #1a1a1a;
+}
+
+.popup_title, .asset_div {
+background-color: #000;
+}
+
+#uploadprogress_container li {
+color: #000;
+}
+
+.asset_div  {
+background-color: #292929;
+border: 1px solid #000;
+}
+
+.recaptchatable #recaptcha_response_field {
+color: #fff;
+}
+
+.blogstats, .bloglastpost {
+background-color: #1a1a1a;
+}
+
+#content_inner {
+background-color: #1a1a1a;
+border: 1px solid #000;
+}
+
+.simpleday {
+background-color: #1a1a1a;
+}
+
+.requestlist > li {
+background-color: #1a1a1a;
+background-image: none;
+border-bottom: 1px solid #202020;
+}
+
+
+li.sgicon .maincol hr, #sidebar li.sgicon {
+border-top: 1px solid #000;
+}
+
+.globalsearch .searchbutton {
+background-color:transparent;
+}
+
+.cms_widget_content_restore_widget_content {
+margin-top: 5px;
+-moz-border-radius: 5px;
+-webkit-border-radius: 5px;
+border-radius: 5px;
+background: #161616 url(images/Seamus/gradients/forumrow1.png) top left repeat-x;
+color: #fff;
+padding: 10px 6px;
+border: 1px solid #000;
+}
+ 
+span.cke_skin_kama {
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_wrapper {
+background: url(images/Seamus/gradients/editor.png) top left repeat-x !important;
+background-color: #202020 !important;
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_toolgroup {
+background-color: #202020 !important;
+background-image: none !important;
+color: #ccc;
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_button a:hover, .cke_skin_kama .cke_button a:focus, .cke_skin_kama .cke_button a:active {
+background-color: #000 !important;
+}
+
+.cke_skin_kama div.cke_bottom_restore_autosave .cke_toolgroup a {
+color: #ccc !important;
+}
+
+.cke_skin_kama .cke_rcombo a, .cke_skin_kama .cke_rcombo a:active, .cke_skin_kama .cke_rcombo a:hover {
+background-image: none !important;
+background-color: #202020 !important;
+border: 1px solid #000 !important;
+color: #ccc !important;
+}
+
+.cke_skin_kama .cke_rcombo .cke_openbutton .cke_icon {
+background-color: #000 !important;
+}
+
+#visitormessageseditor .blockrow {
+border-top: 0 !important;
+}
+
+.cke_panel_listItem a:hover, .cke_panel_listItem a:focus, .cke_panel_listItem a:active {
+background-color: #202020 !important;
+border: 1px solid #000 !important;
+}
+
+.cke_panel_listItem a {
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama *, .cke_skin_kama a:hover, .cke_skin_kama a:link, .cke_skin_kama a:visited, .cke_skin_kama a:active {
+color: #fff !important;
+}
+
+.cke_skin_kama .cke_rcombopanel {
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_button a.cke_on {
+background-color: #6c6e70 !important;
+}
+
+
+.cke_skin_kama .cke_dialog_body {
+background-color: #202020 !important;
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_dialog_title {
+border-bottom: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_dialog_contents {
+background-color: #1a1a1a !important;
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_dialog_footer {
+background-color: #202020 !important;
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama .cke_dialog .cke_dark_background {
+background-color: transparent !important;
+}
+
+.cke_skin_kama a.cke_smile img {
+border: none !important;
+}
+
+#uploadprogress_container {
+background-color: #202020 !important;
+}
+
+.uploadprogress_footer {
+border: 1px solid #000 !important;
+background-color: #222629 !important;
+}
+
+#uploadprogress_container {
+border: 1px solid #000 !important;
+}
+
+.cke_skin_kama a.cke_dialog_tab {
+background-color: #202020 !important;
+background-image: none !important;
+}
+
+.cke_skin_kama a.cke_dialog_tab:hover {
+background-color: #000 !important;
+background-image: none !important;
+}
+
+.cke_skin_kama input.cke_dialog_ui_input_text, .cke_skin_kama input.cke_dialog_ui_input_password {
+background-color: #222629 !important;
+}
+
+.cke_skin_kama .cke_dialog_ui_input_select {
+background-color: #222629 !important;
+}
+
+.cke_panel_listItem a {
+color: #000 !important;
+}
+
+ul.cke_panel_list {
+color: #000 !important;
+}
+
+#visitor_message_inlinemod_form {
+margin-top: 15px;
+}
+.postbit .posttitle, .postbitlegacy .title {
+border-bottom: 1px solid #2A2A2A;
+}
+
+/* Remove everything below this line if you want to use custom profiles */
+
+/* changing profile page - right side */
+div.userprof_content {background: #1a1a1a;} /* right main background color */
+#tab_container dl.tabslight dd.userprof_module a {color: #fff;} /* active tab font color */
+#tab_container dl.tabslight dd.userprof_module {background: #202020;} /* active tab background color */
+#member_content dl.tabslight dd.userprof_moduleinactive a {color: #ccc;} /* inactive tab font color */
+#member_content dl.tabslight dd.userprof_moduleinactive {background: black;} /* inactive tab background color */
+#profile_tabs .memberprofiletabunder {background: #555555;} /* thin line under tabs */
+#message_form div.userprof_headers {background: #202020;} /* around VM message area */
+div.group .userprof_button {background: #000;} /* VM editor button background */
+div.userprof_headers {background: #202020;} /* right header background color */
+ol.userprof .postbit.postbit-lite {background: transparent;} /* right avatar area background color */
+div#sidebar_container.member_summary {background: #1a1a1a;} /* left background */
+#member_content #sidebar_container .blocksubhead {color: #fff;} /* username & Friends & Albums color */
+#sidebar_container.member_summary .mainblock .usertitle {color: #ccc;} /* usertitle */
+#member_content #sidebar_container a {color: #fff;} /* left link color */
+#profile_customization_button.userprof_button {color: green;} /* customize profile button text color */
+.member_summary dd.avatar img#user_avatar {border: none;} /* avatar border */
+.userprof_button {color: #fff !important; background-color: #202020 !important}
+.subsection {padding: 10px}
+.profile_content .subsectionhead, #postlist .subsectionhead { font-size: 13px !important;}
+.profile_content .subsubsectionhead {font-size: 12px !important;}
+.userprof_headers {
+color: #eee !important;
+}
+
+.userprof .postbit .postdate .time {
+color: #eee !important;
+}
